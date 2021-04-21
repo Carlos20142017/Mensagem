69a4443e1c6c574858c92a20f0f99a530038a230
Carlos Borja <carlossocegadocruz@gmail.com>
2021-04-21 11:45:00 +0100

    Layaut Receber Mensagem

--- app/src/main/java/ipg/primeiro/mensagem/MainActivity.kt
+++ app/src/main/java/ipg/primeiro/mensagem/MainActivity.kt
@@ -9,28 +9,11 @@ import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var botao: Button
-    lateinit var editTextEntrada: EditText
-    lateinit var textViewSaida: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        botao= findViewById(R.id.buttonEnviar)
-        editTextEntrada= findViewById(R.id.editTextTextNome)
-        textViewSaida= findViewById(R.id.textViewSaida)
-
-        botao.setOnClickListener(object: View.OnClickListener {
-            override fun onClick(v: View?) {
-                mostrar()
-            }
-
-        })
-
-
 }
-    fun mostrar(){
-        val texto = editTextEntrada.toString()
-        textViewSaida.setText(texto)
-    }
+
 }
---
+++ app/src/main/java/ipg/primeiro/mensagem/MostrarMensagemActivity.kt
@@ -0,0 +1,11 @@
+package ipg.primeiro.mensagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MostrarMensagemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mostrar_mensagem)
+    }
+}
